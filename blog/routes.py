18475f17d28d18ee27de800103162f4d08636a36
Kirill <xaritonov.xka-95@mail.ru>
2019-09-15 20:40:11 +0300

    Create home page and about page

---
+++ blog/routes.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from blog import app
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
